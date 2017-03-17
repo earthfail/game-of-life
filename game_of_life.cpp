eac22950bfe1d7c59860789fb7163193fe5d79e9
Salim Khatib <saleemkhatib22@gmail.com>
2017-03-17 17:34:29 +0200

    fix bug in toad pattern and and consoles which do not support squares

--- game_of_life.cpp
+++ game_of_life.cpp
@@ -1,13 +1,25 @@
-#include <iostream>
+#include<iostream>
 #include<fstream>
 #include<cstdlib> //for cleaning the console
+#include<stdio.h>
+#include<string>
 //TIL: code::blocks take the definition of a function from where it executes
-/*define the width and hight of the board
+/*define the width and hight of the board that suits my terminal
   for future:know how to get the height and width of the window
   or better know how to use buffers in opengl and print stuff in them
   */
-#define HEIGHT 12
-#define WIDTH 52
+#define HEIGHT 38
+#define WIDTH 100
+// cmd can print square but it is not supported on my ubuntu computer
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+#define DASH static_cast<char>(22)
+#define CELL static_cast<char>(254)
+#define CLEAR "cls"
+#else
+#define DASH '-'
+#define CELL '*'
+#define CLEAR "clear"
+#endif
 enum State{DEAD,ALIVE};//0 and 1 is converted to false and true
 namespace board
 {
@@ -30,35 +42,62 @@ int main()
 {
     bool *mainboard= new bool[HEIGHT*WIDTH],*secboard=new bool[HEIGHT*WIDTH],
     *haltboard=new bool[HEIGHT*WIDTH];
-    std::ifstream coordination("lwss.txt");
-    int iterations=100,i=0;
-    if(!board::get_file(coordination,mainboard))//something is wrong
-    {
-        delete[] mainboard;
-        delete[] secboard;
-        delete[] haltboard;
+    std::ifstream coordination;
+    std::string choices;
+    int iterations=100,i;
+    char choice;
+    do
+      {
+        i=0;//give the player 100 move
+        std::cout<<"options: "<<"(g)lider  "<<"(l)ightweight spaceship  "
+                 <<"(b)eacon  "<<"bee(h)ive  "<<"(t)oad  "<<"(e)xit"<<std::endl;
+        std::cin>>choice;
+        std::cin.ignore(10,'\n');
+        switch(choice)
+          {
+          case 'g': choices="glider.txt"; break;
+          case 'l': choices="lwss.txt"; break;
+          case 'b': choices="beacon.txt"; break;
+          case 'h': choices="beehive.txt"; break;
+          case 't': choices="toad.txt"; break;
+          case 'e': std::cout<<"Game over"<<std::endl; break;
+          default:  choices="-";
+            std::cout<<"Please enter a valid input"<<std::endl;
+          }
+        if(choice=='e') break;//end of game
+        if(choices[0]=='-') continue;//loop another time
+        coordination.open(choices.c_str());
+        if(!board::get_file(coordination,mainboard))//something is wrong
+          {
+            delete[] mainboard;
+            delete[] secboard;
+            delete[] haltboard;
+            coordination.close();
+            return 1;
+          }
+        while(i<iterations)//show the board and update it
+          {
+            board::show(mainboard);
+            board::copy(haltboard,mainboard);//saving the old board
+
+            board::update(mainboard,secboard);
+            board::copy(mainboard,secboard);
+
+            if(board::compare(mainboard,haltboard))//the board didn't change
+              break;
+            choice=static_cast<char>(getchar());//to delay the results
+            if(choice=='e') {choice='-'; break;}//the player wants a new game
+            //system(CLEAR);//clear the console *NOT RECOMMENDED
+            i++;
+          }
+        std::cout<<"iterations:"<<i<<std::endl;
+        //close the file where the pattern coordinates to use the variable ifstream again
         coordination.close();
-    }
-    while(i<iterations)
-    {
-        board::show(mainboard);
-        board::copy(haltboard,mainboard);//saving the old board
-
-        board::update(mainboard,secboard);
-        board::copy(mainboard,secboard);
-
-        if(board::compare(mainboard,haltboard))//the board didn't change
-            break;
-        getchar();//to delay the results
-        system("cls");//clear the console *NOT RECOMMENDED
-        i++;
-    }
-    std::cout<<"iterations:"<<i<<std::endl;
+      }while(choice!='e');
   //clean up the memory
   delete[] mainboard;
   delete[] secboard;
   delete[] haltboard;
-  coordination.close();//still don't know if I should close a stream
 
   return 0;
 }
@@ -85,7 +124,7 @@ bool board::get_file(std::ifstream& coordinates,bool *b1,int height,int width)
 }
 void board::show(const bool* b1,int height,int width)
 {
-    unsigned char dash=static_cast<char>(22),square=static_cast<char>(254);
+    unsigned char dash=DASH,cell=CELL;
     //for more professional work.
   for(int i=0;i<height;i++)
     {
@@ -95,7 +134,7 @@ void board::show(const bool* b1,int height,int width)
 
     for(int j=0;j<width;j++)//bound the cell from left
         if(b1[i*width+j]==ALIVE)
-            std::cout<<"|"<<square;
+            std::cout<<"|"<<cell;
         else
             std::cout<<"| ";
 
